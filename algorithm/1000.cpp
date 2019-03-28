760b19dfbbbfcd195ff7a8ac77d1917c19c17041
anetfotoa <anetfotoa@naver.com>
2019-03-28 23:24:22 +0900

    1

---
+++ algorithm/1000.cpp
@@ -0,0 +1,13 @@
+﻿// algorithm.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	int b;
+	int sum = 0;
+	cin >> a >> b;
+	cout << a + b << "\n";
+}
